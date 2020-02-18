3bc0bea17183f560fd417131e9c96d57e89ff935
Juan Cuartas <equisoide@gmail.com>
2020-02-18 10:49:15 -0500

    created CodeAttribute utility class

---
+++ source/Celerik.NetCore.Util/Attributes/CodeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Defines a custom Code attribute.
+    /// </summary>
+    public class CodeAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the class.
+        /// </summary>
+        /// <param name="code">Code value.</param>
+        public CodeAttribute(string code)
+        {
+            Code = code;
+        }
+
+        /// <summary>
+        /// Gets or sets the Code value.
+        /// </summary>
+        public string Code { get; private set; }
+    }
+}
