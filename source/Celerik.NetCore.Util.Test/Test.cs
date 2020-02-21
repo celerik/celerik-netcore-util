4962cc0d49ece539e2b681d72836b75930f577aa
Juan Cuartas <equisoide@gmail.com>
2020-02-21 09:38:05 -0500

    created test method

---
+++ source/Celerik.NetCore.Util.Test/Test.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void Test_1_Equals_1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
