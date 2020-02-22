d477e5b12af2d40ecbf184f7ef840ba6b917f322
Juan Cuartas <equisoide@gmail.com>
2020-02-21 23:38:45 -0500

    refactored the only test into the Util.Test project

---
+++ source/Celerik.NetCore.Util.Test/FirstTest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class FirstTest
+    {
+        [TestMethod]
+        public void TestAssert() => Assert.AreEqual(1, 1);
+    }
+}
