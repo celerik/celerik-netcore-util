4529b8b7e2e354740a89406f1ae2e6ad20ed221a
Juan Cuartas <equisoide@gmail.com>
2020-03-02 17:11:17 -0500

    Added Crypto utility class

---
+++ source/Celerik.NetCore.Util/Utilities/CryptoUtility.cs
@@ -0,0 +1,34 @@
+﻿using CryptoHelper;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Provides cryptography utilities.
+    /// </summary>
+    public static class CryptoUtility
+    {
+        /// <summary>
+        /// Returns a hashed representation of the specified password.
+        /// </summary>
+        /// <param name="password">The password to generate a hash value for.</param>
+        /// <returns>The hash value for password as a base-64-encoded string.</returns>
+        /// <exception cref="System.ArgumentNullException">Password is null.</exception>
+        public static string HashPassword(string password) =>
+            Crypto.HashPassword(password);
+
+        /// <summary>
+        /// Determines whether the specified RFC 2898 hash and password are a
+        /// cryptographic match.
+        /// </summary>
+        /// <param name="hash">The previously-computed RFC 2898 hash value as a
+        /// base-64-encoded string.</param>
+        /// <param name="password">The plaintext password to cryptographically
+        /// compare with hashedPassword.</param>
+        /// <returns>True if the hash value is a cryptographic match for the
+        /// password; otherwise, false.</returns>
+        /// <exception cref="System.ArgumentNullException">HashedPassword or password
+        /// is null.</exception>
+        public static bool VerifyHashedPassword(string hash, string password) =>
+            Crypto.VerifyHashedPassword(hash, password);
+    }
+}
