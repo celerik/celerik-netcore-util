da90febed608b8d438f4a68bdb09eb70b1329b7c
Juan Cuartas <equisoide@gmail.com>
2020-03-02 13:29:09 -0500

    Create file utilities (#33)
    * Created utility SerializeAllContractResolver class
    
    * Created FileUility class
    
    * Created EmbeddedFileUtilityClass
    
    * Updated nuget package info

---
+++ source/Celerik.NetCore.Util/Files/EmbeddedFileUtility.cs
@@ -0,0 +1,96 @@
+﻿using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Provides embedded file utilities.
+    /// </summary>
+    public static class EmbeddedFileUtility
+    {
+        /// <summary>
+        /// Reads the content of an embedded file existing into the
+        /// passed-in assembly.
+        /// </summary>
+        /// <param name="name">The case-sensitive name of the manifest resource
+        /// being requested without including the assembly namespace.
+        /// E.g.: "FolderName.FileName.extension".</param>
+        /// <param name="assembly">The assembly where we get the resource. If
+        /// null, we search the resource into the calling assembly.</param>
+        /// <returns>Content of the embedded file existing into the
+        /// passed-in assembly.</returns>
+        /// <exception cref="FileNotFoundException">If the embedded file could not
+        /// be found.</exception>
+        public static string ReadFile(string name, Assembly assembly = null)
+        {
+            try
+            {
+                if (assembly == null)
+                    assembly = Assembly.GetCallingAssembly();
+
+                var namespce = assembly.FullName.Split(',')[0];
+                var fullPath = $"{namespce}.{name}";
+
+                using var stream = assembly.GetManifestResourceStream(fullPath);
+                using var reader = new StreamReader(stream);
+                var content = reader.ReadToEnd();
+
+                return content;
+            }
+            catch
+            {
+                throw new FileNotFoundException(UtilResources.Get(
+                    "EmbeddedFileUtility.ReadFileException", name
+                ));
+            }
+        }
+
+        /// <summary>
+        /// Reads the content of an embedded JSON file existing into the
+        /// calling assembly.
+        /// </summary>
+        /// <typeparam name="TOutputType">The type to which the JSON file will be
+        /// converted.</typeparam>
+        /// <param name="name">The case-sensitive name of the manifest resource
+        /// being requested without including the assembly namespace.
+        /// E.g.: "FolderName.FileName.json".</param>
+        /// <param name="serializeAll">Indicates whether all props should be
+        /// serialized without taking into account the JsonIgnore attribute.</param>
+        /// <returns>Content of the embedded JSON file existing into the
+        /// calling assembly, casted to the specified Type.</returns>
+        /// <exception cref="FileNotFoundException">If the embedded JSON file could not
+        /// be found.</exception>
+        /// <exception cref="FileLoadException">If there was an error trying to load
+        /// the content of the JSON file.</exception>
+        public static TOutputType ReadJson<TOutputType>(string name, bool serializeAll = true)
+        {
+            var content = ReadFile(name, Assembly.GetCallingAssembly());
+
+            try
+            {
+                if (serializeAll)
+                {
+                    var settings = new JsonSerializerSettings
+                    {
+                        ContractResolver = new SerializeAllContractResolver()
+                    };
+
+                    var obj = JsonConvert.DeserializeObject<TOutputType>(content, settings);
+                    return obj;
+                }
+                else
+                {
+                    var obj = JsonConvert.DeserializeObject<TOutputType>(content);
+                    return obj;
+                }
+            }
+            catch
+            {
+                throw new FileLoadException(UtilResources.Get(
+                    "EmbeddedFileUtility.ReadJsonException", name, typeof(TOutputType)
+                ));
+            }
+        }
+    }
+}
---
+++ source/Celerik.NetCore.Util/NewtonSoft/SerializeAllContractResolver .cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Contract resolver to serialize all props in an object not taking into
+    /// account the JsonIgnore attribute.
+    /// </summary>
+    /// <code>
+    ///     var settings = new JsonSerializerSettings
+    ///     {
+    ///         ContractResolver = new SerializeAllContractResolver()
+    ///     };
+    ///     
+    ///     var json = JsonConvert.SerializeObject(obj, settings);
+    /// </code>
+    public class SerializeAllContractResolver : DefaultContractResolver
+    {
+        /// <summary>
+        /// Creates properties for the given Newtonsoft.Json.Serialization.JsonContract.
+        /// </summary>
+        /// <param name="type">The type to create properties for.</param>
+        /// <param name="memberSerialization">The member serialization mode for the type.
+        /// </param>
+        /// <returns>Properties for the given Newtonsoft.Json.Serialization.JsonContract.
+        /// </returns>
+        protected override IList<JsonProperty> CreateProperties(
+            Type type, MemberSerialization memberSerialization)
+        {
+            var props = base.CreateProperties(type, memberSerialization);
+
+            foreach (var prop in props)
+                prop.Ignored = false;
+
+            return props;
+        }
+    }
+}
