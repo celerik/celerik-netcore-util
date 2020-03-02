e8df8f6918fa7a5c17c0f7cd78233ed779683700
Juan Cuartas <equisoide@gmail.com>
2020-03-02 11:21:26 -0500

    Created FileUility class

---
+++ source/Celerik.NetCore.Util/Files/FileUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Provides file utilities.
+    /// </summary>
+    public static class FileUtility
+    {
+        /// <summary>
+        /// Gets the full path of the current assembly.
+        /// </summary>
+        /// <returns>Full path of the current assmbly.</returns>
+        public static string GetAssemblyDirectory()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var codeBase = assembly.CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
+            var directory = Path.GetDirectoryName(path);
+
+            return directory;
+        }
+    }
+}
