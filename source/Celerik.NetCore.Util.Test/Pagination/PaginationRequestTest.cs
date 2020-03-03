3d1824b7a97ef331f76116a7dd18b2cd52fc2745
Juan Cuartas <equisoide@gmail.com>
2020-03-03 10:26:45 -0500

    Create testing for pagination utility [norelease] (#54)
    * Updated project information
    
    * Removed FirstTest
    
    * Created UtilBaseTest
    
    * Created PaginationExtensionsTest
    
    * Created PaginationRequestTest
    
    * Created PaginationRequestValidatorTest
    
    * Updated tutorials.md with GitHub release actions info

---
+++ source/Celerik.NetCore.Util.Test/Pagination/PaginationRequestTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class PaginationRequestTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void SortAscending()
+        {
+            var request = new PaginationRequest
+            {
+                PageNumber = 1,
+                PageSize = 20,
+                SortKey = "Name",
+                SortDirection = "asc"
+            };
+
+            Assert.AreEqual(true, request.IsAscending);
+        }
+
+        [TestMethod]
+        public void SortDescending()
+        {
+            var request = new PaginationRequest
+            {
+                PageNumber = 1,
+                PageSize = 20,
+                SortKey = "Name",
+                SortDirection = "desc"
+            };
+
+            Assert.AreEqual(false, request.IsAscending);
+        }
+
+        [TestMethod]
+        public void DefaultSort()
+        {
+            var request = new PaginationRequest
+            {
+                PageNumber = 1,
+                PageSize = 20,
+                SortKey = "Name"
+            };
+
+            Assert.AreEqual(true, request.IsAscending);
+        }
+    }
+}
