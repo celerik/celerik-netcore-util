fb5b7d5fc0d4df0f30f55c3c2c8cb1b25a19e2d1
Juan Cuartas <equisoide@gmail.com>
2020-03-03 10:12:48 -0500

    Created UtilBaseTest

---
+++ source/Celerik.NetCore.Util.Test/UtilBaseTest.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Celerik.NetCore.Util.Test
+{
+    public class UtilBaseTest : BaseTest
+    {
+        protected override void AddServices(IServiceCollection services)
+        {
+            services.AddSingleton<IValidator<PaginationRequest>, PaginationRequestValidator<PaginationRequest>>();
+            services.AddSingleton<IValidator<PaginationRequest<Cat>>, PaginationRequestValidator<PaginationRequest, Cat>>();
+        }
+    }
+}
