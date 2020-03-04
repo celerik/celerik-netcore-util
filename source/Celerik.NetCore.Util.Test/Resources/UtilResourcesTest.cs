2b3b75b7247ee54377de1293766fe8ba441041e3
Juan Cuartas <equisoide@gmail.com>
2020-03-04 14:38:21 -0500

    Created test for UtilResources

---
+++ source/Celerik.NetCore.Util.Test/Resources/UtilResourcesTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class UtilResourcesTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            var name = "The database was deleted!";
+            var resource = UtilResources.Get(name);
+
+            Assert.AreEqual(name, resource);
+        }
+
+        [TestMethod]
+        public void GetWithArgs()
+        {
+            var name = "The {0} database couldn´t be deleted!";
+            var args = "ChuckNorrisFacts";
+            var resource = UtilResources.Get(name, args);
+
+            Assert.AreEqual("The ChuckNorrisFacts database couldn´t be deleted!", resource);
+        }
+    }
+}
