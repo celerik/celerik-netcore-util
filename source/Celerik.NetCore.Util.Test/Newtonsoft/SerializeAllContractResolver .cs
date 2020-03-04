814de8cf2c3297cfc3937c1ff1160bba95d8fc1c
Juan Cuartas <equisoide@gmail.com>
2020-03-04 14:32:22 -0500

    Create testing for newtonsoft utilities (#70)
    * Fixed nuget-deploy
    
    * Created test for EnumDescriptionJsonConverter
    
    * Create test for SerializeAllContractResolver
    
    [release]

---
+++ source/Celerik.NetCore.Util.Test/Newtonsoft/SerializeAllContractResolver .cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Celerik.NetCore.Util.Test
+{
+    public class Metallica
+    {
+        public string JamesHetfield { get; set; }
+        public string KirkHammett { get; set; }
+        public string CliffBurton { get; set; }
+
+        [JsonIgnore]
+        public string LarsUlrich { get; set; }
+    }
+
+    [TestClass]
+    public class SerializeAllContractResolverTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void WithoutContractResolver()
+        {
+            var metallica = new Metallica
+            {
+                JamesHetfield = "Cool guy",
+                KirkHammett = "Cool guy",
+                CliffBurton = "Cool guy",
+                LarsUlrich = "Sucks",
+            };
+
+            var json = JsonConvert.SerializeObject(metallica);
+
+            Assert.AreEqual(true, json.Contains("\"JamesHetfield\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, json.Contains("\"KirkHammett\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, json.Contains("\"CliffBurton\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(false, json.Contains("\"LarsUlrich\":", StringComparison.InvariantCulture));
+        }
+
+        [TestMethod]
+        public void WithContractResolver()
+        {
+            var metallica = new Metallica
+            {
+                JamesHetfield = "Cool guy",
+                KirkHammett = "Cool guy",
+                CliffBurton = "Cool guy",
+                LarsUlrich = "Sucks"
+            };
+
+            var settings = new JsonSerializerSettings
+            {
+                ContractResolver = new SerializeAllContractResolver()
+            };
+
+            var json = JsonConvert.SerializeObject(metallica, settings);
+
+            Assert.AreEqual(true, json.Contains("\"JamesHetfield\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, json.Contains("\"KirkHammett\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, json.Contains("\"CliffBurton\":", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, json.Contains("\"LarsUlrich\":", StringComparison.InvariantCulture));
+        }
+    }
+}
