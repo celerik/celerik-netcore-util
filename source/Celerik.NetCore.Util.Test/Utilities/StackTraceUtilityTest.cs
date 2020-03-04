e1594ca449ec1a8c65d1a71f7a5ae9feda5d6184
Juan Cuartas <equisoide@gmail.com>
2020-03-04 17:52:35 -0500

    Created test for StackTrace utility (#82)

---
+++ source/Celerik.NetCore.Util.Test/Utilities/StackTraceUtilityTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class StackTraceUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void GetMethodName()
+        {
+            var stackTrace = new StackTrace();
+            var topMethodName = stackTrace.GetMethodName();
+
+            Assert.AreEqual("StackTraceUtilityTest.GetMethodName()", topMethodName);
+        }
+    }
+}
