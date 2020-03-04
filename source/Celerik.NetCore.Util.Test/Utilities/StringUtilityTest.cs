63bf40eb6e9aa87f2d41fddcb0ff2e4af2682103
Juan Cuartas <equisoide@gmail.com>
2020-03-04 17:56:24 -0500

    Created tests for String utilities

---
+++ source/Celerik.NetCore.Util.Test/Utilities/StringUtilityTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class StringUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void FirstUpperRestLower()
+        {
+            Assert.AreEqual(null, ((string)null).FirstUpperRestLower());
+            Assert.AreEqual(string.Empty, string.Empty.FirstUpperRestLower());
+            Assert.AreEqual("A", "A".FirstUpperRestLower());
+            Assert.AreEqual("A", "a".FirstUpperRestLower());
+            Assert.AreEqual("Abc", "ABC".FirstUpperRestLower());
+            Assert.AreEqual("Abc", "abc".FirstUpperRestLower());
+        }
+    }
+}
