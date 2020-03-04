014f2f76b95cf50433d4330094dd15be28b34de3
Juan Cuartas <equisoide@gmail.com>
2020-03-04 14:11:08 -0500

    Create testing for files utilities (#68)
    * Created EmmbedFileUtilityTest
    
    * Created FileUtilityTest
    
    * Created Movies.json file
    
    * Updated Celerik.NetCore.Util.csproj
    
    * Fixed test in EmbeddedFileUtilityTest
    
    * Updated test ptoject information
    
    * Updated tutorials.md file with Context and expression syntax for GitHub Actions
    
    [release]

---
+++ source/Celerik.NetCore.Util.Test/Files/EmbeddedFileUtilityTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Celerik.NetCore.Util.Test
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [JsonIgnore]
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class EmbeddedFileUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ReadFileFileNotFoundException()
+        {
+            EmbeddedFileUtility.ReadFile("Files.UnexistingFile.txt");
+        }
+
+        [TestMethod]
+        public void ReadFileOk()
+        {
+            var movies = EmbeddedFileUtility.ReadFile("Files.Movies.json");
+
+            Assert.AreEqual(true, movies.Contains("Enter the Dragon", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, movies.Contains("Kill Bill", StringComparison.InvariantCulture));
+            Assert.AreEqual(true, movies.Contains("Terminator", StringComparison.InvariantCulture));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void ReadJsonFileNotFoundException()
+        {
+            EmbeddedFileUtility.ReadJson<object>("Files.UnexistingFile.json");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileLoadException))]
+        public void ReadJsonFileLoadException()
+        {
+            EmbeddedFileUtility.ReadJson<EmbeddedFileUtilityTest>("Files.Movies.json");
+        }
+
+        [TestMethod]
+        public void ReadJsonSerializeAllOk()
+        {
+            var movies = EmbeddedFileUtility.ReadJson<IEnumerable<Movie>>("Files.Movies.json");
+
+            Assert.AreEqual(1, movies.ElementAt(0).Id);
+            Assert.AreEqual(2, movies.ElementAt(1).Id);
+            Assert.AreEqual(3, movies.ElementAt(2).Id);
+
+            Assert.AreEqual("Enter the Dragon", movies.ElementAt(0).Name);
+            Assert.AreEqual("Kill Bill", movies.ElementAt(1).Name);
+            Assert.AreEqual("Terminator 2", movies.ElementAt(2).Name);
+        }
+
+        [TestMethod]
+        public void ReadJsonApplyJsonIgnoreOk()
+        {
+            var movies = EmbeddedFileUtility.ReadJson<IEnumerable<Movie>>(
+                "Files.Movies.json", serializeAll: false
+            );
+
+            Assert.AreEqual(1, movies.ElementAt(0).Id);
+            Assert.AreEqual(2, movies.ElementAt(1).Id);
+            Assert.AreEqual(3, movies.ElementAt(2).Id);
+
+            Assert.AreEqual(null, movies.ElementAt(0).Name);
+            Assert.AreEqual(null, movies.ElementAt(1).Name);
+            Assert.AreEqual(null, movies.ElementAt(2).Name);
+        }
+    }
+}
