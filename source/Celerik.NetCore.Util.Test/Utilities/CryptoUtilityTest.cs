995c52fa8854dad8ce3b060054287068f2413314
Juan Cuartas <equisoide@gmail.com>
2020-03-04 15:49:07 -0500

    Created test for CryptoUtility (#76)

---
+++ source/Celerik.NetCore.Util.Test/Utilities/CryptoUtilityTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class CryptoUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void HashPassword()
+        {
+            var password = "AlexaPlayLaTigresaDelOriente";
+            var hash1 = CryptoUtility.HashPassword(password);
+            var hash2 = CryptoUtility.HashPassword(password);
+
+            Assert.AreEqual(true, hash1.Length > 0);
+            Assert.AreEqual(true, hash2.Length > 0);
+            Assert.AreNotEqual(hash1, hash2);
+
+            TestUtility.AssertThrows<ArgumentNullException>(() =>
+            {
+                CryptoUtility.HashPassword(null);
+            });
+        }
+
+        [TestMethod]
+        public void VerifyHashedPassword()
+        {
+            var password = "AlexaPlayLaPequenaWendyZulca";
+            var hash = CryptoUtility.HashPassword(password);
+            var isValidHash = CryptoUtility.VerifyHashedPassword(hash, password);
+
+            Assert.AreEqual(true, isValidHash);
+
+            TestUtility.AssertThrows<ArgumentNullException>(() =>
+            {
+                CryptoUtility.VerifyHashedPassword(null, null);
+            });
+        }
+    }
+}
