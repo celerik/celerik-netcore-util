ef9745f8661ad1a3ec6f95a52ea4ada8c66aac37
Juan Cuartas <equisoide@gmail.com>
2020-03-04 11:36:15 -0500

    Created FileUtilityTest

---
+++ source/Celerik.NetCore.Util.Test/Files/FileUtilityTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    [TestClass]
+    public class FileUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        public void FileUtilityGetAssemblyDirectory()
+        {
+            var directory = FileUtility.GetAssemblyDirectory();
+            Assert.AreNotEqual(null, directory);
+        }
+    }
+}
