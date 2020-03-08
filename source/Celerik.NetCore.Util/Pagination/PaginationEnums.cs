0806404f930115ab53c7e1f427c565c35db610df
Juan Cuartas <equisoide@gmail.com>
2020-03-08 09:44:04 -0500

    Added xml documentation to SortDirectionType enum

--- source/Celerik.NetCore.Util/Pagination/PaginationEnums.cs
+++ source/Celerik.NetCore.Util/Pagination/PaginationEnums.cs
@@ -2,11 +2,20 @@
 
 namespace Celerik.NetCore.Util
 {
+    /// <summary>
+    /// Defines the possible types of sorting.
+    /// </summary>
     public enum SortDirectionType
     {
+        /// <summary>
+        /// Sorting ascending.
+        /// </summary>
         [Description("asc")]
         Asc = 1,
 
+        /// <summary>
+        /// Sorting descending.
+        /// </summary>
         [Description("desc")]
         Desc = 2
     }
