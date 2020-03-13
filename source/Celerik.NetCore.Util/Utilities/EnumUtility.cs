6eeb21403fb114f86a49383611e95cf070f525d1
Juan Cuartas <equisoide@gmail.com>
2020-03-13 11:21:41 -0500

    Refactored enum utility

--- source/Celerik.NetCore.Util/Utilities/EnumUtility.cs
+++ source/Celerik.NetCore.Util/Utilities/EnumUtility.cs
@@ -13,15 +13,140 @@ namespace Celerik.NetCore.Util
     public static class EnumUtility
     {
         /// <summary>
+        /// Gets an attribute belonging to this enumeration. Returns null if the
+        /// attribute does not exists.
+        /// </summary>
+        /// <typeparam name="TAttribute">The attribute type to get.</typeparam>
+        /// <param name="value">Enum value.</param>
+        /// <returns>Attribute belonging to this enumeration.</returns>
+        public static TAttribute GetAttribute<TAttribute>(this Enum value)
+            where TAttribute : Attribute
+        {
+            if (value == null)
+                throw new ArgumentException(UtilResources.Get("EnumUtility.GetAttribute.NullValue"));
+
+            var type = typeof(TAttribute);
+            var memberInfo = value.GetType().GetMember(value.ToString());
+
+            if (memberInfo.Length > 0)
+                return memberInfo[0].GetCustomAttributes(type, inherit: false)
+                    .OfType<TAttribute>().FirstOrDefault();
+
+            return null;
+        }
+
+        /// <summary>
+        /// Gets the CodeAttribute value of this enumeration.
+        /// </summary>
+        /// <param name="value">Enum value.</param>
+        /// <param name="defaultVal">Value to return by default in case the enumeration
+        /// does not have a CodeAttribute.</param>
+        /// <returns>Code of this enumeration.</returns>
+        public static string GetCode(this Enum value, string defaultVal = null) =>
+            value.GetAttribute<CodeAttribute>()?.Code ??
+            defaultVal;
+
+        /// <summary>
+        /// Gets the CodeAttribute value of an enumeration matching the
+        /// passed-in value.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
+        /// <param name="value">Enum value.</param>
+        /// <param name="defaultVal">Value to return by default in case the value doesn't
+        /// exist in the enum.</param>
+        /// <returns>Code of the enumeration.</returns>
+        /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
+        public static string GetCode<TEnum>(int value, string defaultVal = null)
+            where TEnum : struct, IConvertible
+        {
+            var type = typeof(TEnum);
+
+            if (!type.IsEnum)
+                throw new InvalidOperationException(UtilResources.Get("EnumUtility.NotAnEnum", type));
+            if (!Enum.IsDefined(typeof(TEnum), value))
+                return defaultVal;
+
+            return ((Enum)Enum.ToObject(type, value)).GetCode(defaultVal);
+        }
+
+        /// <summary>
+        /// Gets the DescriptionAttribute value of this enumeration. If the
+        /// enumeration does not have a DescriptionAttribute, the enum name is
+        /// returned.
+        /// </summary>
+        /// <param name="value">Enum value.</param>
+        /// <returns>Description of this enumeration.</returns>
+        public static string GetDescription(this Enum value) =>
+            value.GetAttribute<DescriptionAttribute>()?.Description ??
+            value.ToString();
+
+        /// <summary>
+        /// Gets the DescriptionAttribute value of an enumeration matching the
+        /// passed-in value.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
+        /// <param name="value">Enum value.</param>
+        /// <param name="defaultVal">Value to return by default in case the value doesn't
+        /// exist in the enum.</param>
+        /// <returns>Description of the enumeration.</returns>
+        /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
+        public static string GetDescription<TEnum>(int value, string defaultVal = null)
+            where TEnum : struct, IConvertible
+        {
+            var type = typeof(TEnum);
+
+            if (!type.IsEnum)
+                throw new InvalidOperationException(UtilResources.Get("EnumUtility.NotAnEnum", type));
+            if (!Enum.IsDefined(typeof(TEnum), value))
+                return defaultVal;
+
+            return ((Enum)Enum.ToObject(type, value)).GetDescription();
+        }
+
+        /// <summary>
+        /// Gets an enum value from its CodeAttribute.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
+        /// <param name="code">Code of the enum value to search.</param>
+        /// <returns>Enum value for the given CodeAttribute.</returns>
+        /// <param name="defaultVal">Value to return by default in case the code
+        /// doesn't exist in the enum.</param>
+        /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
+        public static TEnum GetValueFromCode<TEnum>(string code, TEnum defaultVal = default)
+            where TEnum : struct, IConvertible
+        {
+            var type = typeof(TEnum);
+
+            if (!type.IsEnum)
+                throw new InvalidOperationException(UtilResources.Get("EnumUtility.NotAnEnum", type));
+
+            foreach (var field in type.GetFields())
+            {
+                if (Attribute.GetCustomAttribute(
+                    field, typeof(CodeAttribute)
+                ) is CodeAttribute attribute)
+                {
+                    if (attribute.Code == code)
+                        return (TEnum)field.GetValue(null);
+                }
+
+                if (field.Name == code)
+                    return (TEnum)field.GetValue(null);
+            }
+
+            return defaultVal;
+        }
+
+        /// <summary>
         /// Gets an enum value from its DescriptionAttribute.
         /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
         /// <param name="description">Description of the enum value to search.</param>
         /// <returns>Enum value for the given DescriptionAttribute.</returns>
-        /// <param name="defaultVal">Value to return in case the description doesn't
-        /// exist in the enum.</param>
+        /// <param name="defaultVal">Value to return by default in case the description
+        /// doesn't exist in the enum.</param>
         /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
-        public static TEnum GetValue<TEnum>(string description, TEnum defaultVal = default)
+        public static TEnum GetValueFromDescription<TEnum>(string description, TEnum defaultVal = default)
             where TEnum : struct, IConvertible
         {
             var type = typeof(TEnum);
@@ -47,98 +172,9 @@ namespace Celerik.NetCore.Util
         }
 
         /// <summary>
-        /// Gets an attribute belonging to this enumeration value. Returns null
-        /// if the attribute does not exists.
-        /// </summary>
-        /// <typeparam name="TAttribute">The attribute type to get.</typeparam>
-        /// <param name="value">Enum value.</param>
-        /// <returns>Attribute belonging to this enumeration value.</returns>
-        public static TAttribute GetAttribute<TAttribute>(this Enum value)
-            where TAttribute : Attribute
-        {
-            if (value == null)
-                throw new ArgumentException(UtilResources.Get("EnumUtility.GetAttribute.NullValue"));
-
-            var type = typeof(TAttribute);
-            var memberInfo = value.GetType().GetMember(value.ToString());
-
-            if (memberInfo.Length > 0)
-                return memberInfo[0].GetCustomAttributes(type, inherit: false)
-                    .OfType<TAttribute>().FirstOrDefault();
-
-            return null;
-        }
-
-        /// <summary>
-        /// Gets the code of this enumeration. In case the
-        /// enumeration does not have a CodeAttribute, the enum
-        /// name is returned.
-        /// </summary>
-        /// <param name="value">Enumeration value.</param>
-        /// <returns>Code of this enumeration.</returns>
-        public static string GetCode(this Enum value) =>
-            value.GetAttribute<CodeAttribute>()?.Code ??
-            value.ToString();
-
-        /// <summary>
-        /// Gets the description of this enumeration. In case the
-        /// enumeration does not have a DescriptionAttribute, the enum
-        /// name is returned.
-        /// </summary>
-        /// <param name="value">Enumeration value.</param>
-        /// <returns>Description of this enumeration.</returns>
-        public static string GetDescription(this Enum value) =>
-            value.GetAttribute<DescriptionAttribute>()?.Description ??
-            value.ToString();
-
-        /// <summary>
-        /// Gets the enum code from its integer value.
-        /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
-        /// <param name="value">Enumeration integer value.</param>
-        /// <param name="defaultVal">Value to return in case the value doesn't
-        /// exist in the enum.</param>
-        /// <returns>Enum code from its integer value.</returns>
-        /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
-        public static string GetCode<TEnum>(int value, string defaultVal = null)
-            where TEnum : struct, IConvertible
-        {
-            var type = typeof(TEnum);
-
-            if (!type.IsEnum)
-                throw new InvalidOperationException(UtilResources.Get("EnumUtility.NotAnEnum", type));
-            if (!Enum.IsDefined(typeof(TEnum), value))
-                return defaultVal;
-
-            return ((Enum)Enum.ToObject(type, value)).GetCode();
-        }
-
-        /// <summary>
-        /// Gets the enum description from its integer value.
-        /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
-        /// <param name="value">Enumeration integer value.</param>
-        /// <param name="defaultVal">Value to return in case the value doesn't
-        /// exist in the enum.</param>
-        /// <returns>Enum description from its integer value.</returns>
-        /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
-        public static string GetDescription<TEnum>(int value, string defaultVal = null)
-            where TEnum : struct, IConvertible
-        {
-            var type = typeof(TEnum);
-
-            if (!type.IsEnum)
-                throw new InvalidOperationException(UtilResources.Get("EnumUtility.NotAnEnum", type));
-            if (!Enum.IsDefined(typeof(TEnum), value))
-                return defaultVal;
-
-            return ((Enum)Enum.ToObject(type, value)).GetDescription();
-        }
-
-        /// <summary>
         /// Gets the minimun value of an enum.
         /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
         /// <returns>Minimun value of the enum.</returns>
         /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
         public static int GetMin<TEnum>()
@@ -155,7 +191,7 @@ namespace Celerik.NetCore.Util
         /// <summary>
         /// Gets the maximun value of an enum.
         /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
         /// <returns>Maximun value of the enum.</returns>
         /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
         public static int GetMax<TEnum>()
@@ -170,9 +206,11 @@ namespace Celerik.NetCore.Util
         }
 
         /// <summary>
-        /// Converts an enum to a list of strings.
+        /// Converts an enum to a list of strings. Description of each enum
+        /// item is based on the DescriptionAttribute, if the enum item does not
+        /// have a DescriptionAttribute, the enum name is took.
         /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
         /// <returns>Enumeration converted to a list of strings.</returns>
         /// <exception cref="InvalidOperationException">TEnum is not an enum.</exception>
         public static List<string> ToList<TEnum>() where TEnum : struct, IConvertible
@@ -204,7 +242,7 @@ namespace Celerik.NetCore.Util
         /// <summary>
         /// Converts an enum to a list of objects.
         /// </summary>
-        /// <typeparam name="TEnum">Enumeration type.</typeparam>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
         /// <typeparam name="TList">List type.</typeparam>
         /// <param name="valueProp">Property name for the vale in the list.</param>
         /// <param name="descriptionProp">Property name for the description in the list.</param>
