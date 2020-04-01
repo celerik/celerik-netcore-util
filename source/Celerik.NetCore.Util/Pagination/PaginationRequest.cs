3b71c9b2b812c6221aba066d7b76f8b1b2a9fa4c
Juan Cuartas <equisoide@gmail.com>
2020-04-01 00:18:54 -0500

    Code refactor, move DocFx folder (#133)
    * Modified documentation pipeline
    
    * Code refactored (sort usings, remove unnecesary code)
    
    * Updated package information
    
    [release]

--- source/Celerik.NetCore.Util/Pagination/PaginationRequest.cs
+++ source/Celerik.NetCore.Util/Pagination/PaginationRequest.cs
@@ -33,7 +33,7 @@ namespace Celerik.NetCore.Util
         [JsonIgnore]
         public bool IsAscending =>
             SortDirection?.ToLowerInvariant() !=
-            Util.SortDirectionType.Desc.GetDescription().ToLowerInvariant();
+            SortDirectionType.Desc.GetDescription().ToLowerInvariant();
     }
 
     /// <summary>
