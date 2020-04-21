ebce7ce4e28dcd66438bbf5cf1e6043a6b580545
Juan Cuartas <equisoide@gmail.com>
2020-04-21 17:07:40 -0500

    Added config utility (#180)
    [release]

---
+++ source/Celerik.NetCore.Util.Test/Utilities/ConfigUtilityTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Celerik.NetCore.Util.Test
+{
+    public class DeveloperSettigs
+    {
+        public string ScreenSize { get; set; }
+        public string CoffeeBrand { get; set; }
+        public string PreferredMusic { get; set; }
+        public string Pet { get; set; }
+    }
+
+    [TestClass]
+    public class ConfigUtilityTest : UtilBaseTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReadFromNullConfiguration()
+        {
+            IConfiguration config = null;
+            config.Read<DeveloperSettigs>("DeveloperSettigs");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigException))]
+        public void ReadKeyNotFound()
+        {
+            var config = GetService<IConfiguration>();
+            config.Read<DeveloperSettigs>("DeveloperSettigs");
+        }
+
+        [TestMethod]
+        public void ReadOk()
+        {
+            var config = GetService<IConfiguration>();
+            config["DeveloperSettigs:ScreenSize"] = "100 inches";
+            config["DeveloperSettigs:CoffeeBrand"] = "Café La Bastilla";
+            config["DeveloperSettigs:PreferredMusic"] = "Ultra super dark satanic bloody metal";
+            config["DeveloperSettigs:Pet"] = "Kitten";
+
+            var settings = config.Read<DeveloperSettigs>("DeveloperSettigs");
+
+            Assert.AreEqual("100 inches", settings.ScreenSize);
+            Assert.AreEqual("Café La Bastilla", settings.CoffeeBrand);
+            Assert.AreEqual("Ultra super dark satanic bloody metal", settings.PreferredMusic);
+            Assert.AreEqual("Kitten", settings.Pet);
+        }
+    }
+}
---
+++ source/Celerik.NetCore.Util/Utilities/ConfigUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace Celerik.NetCore.Util
+{
+    /// <summary>
+    /// Provides configuration utilities.
+    /// </summary>
+    public static class ConfigUtility
+    {
+        /// <summary>
+        /// Attempts to create a T instance by matching property names against
+        /// configuration keys recursively.
+        /// </summary>
+        /// <typeparam name="T">The type of the instance to be created.</typeparam>
+        /// <param name="config">Reference to the current IConfiguration instance.
+        /// </param>
+        /// <param name="key">The key of the configuration section.</param>
+        /// <returns>Instance of type T.</returns>
+        public static T Read<T>(this IConfiguration config, string key)
+        {
+            if (config == null)
+                throw new ArgumentNullException(
+                    UtilResources.Get("Common.ArgumentCanNotBeNull", nameof(config)));
+
+            var configSection = config.GetSection(key);
+            if (!configSection.Exists())
+                throw new ConfigException(
+                    UtilResources.Get("Common.ConfigurationKeyNotFound", key)
+                );
+
+            var instance = (T)Activator.CreateInstance(typeof(T));
+            configSection.Bind(instance);
+
+            return instance;
+        }
+    }
+}
