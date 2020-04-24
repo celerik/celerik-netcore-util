8c2801a60869583335743d755d0141caf7cbc99c
Juan Cuartas <equisoide@gmail.com>
2020-04-24 13:50:55 -0500

    Added validations, string resources and config utilities

--- source/Celerik.NetCore.Util.Test/Utilities/ConfigUtilityTest.cs
+++ source/Celerik.NetCore.Util.Test/Utilities/ConfigUtilityTest.cs
@@ -12,27 +12,33 @@ namespace Celerik.NetCore.Util.Test
         public string Pet { get; set; }
     }
 
+    public enum GuitarBrandType
+    {
+        Gibson = 1,
+        Cheapson = 2
+    }
+
     [TestClass]
     public class ConfigUtilityTest : UtilBaseTest
     {
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
-        public void ReadFromNullConfiguration()
+        public void ReadObjectFromNullConfiguration()
         {
             IConfiguration config = null;
-            config.Read<DeveloperSettigs>("DeveloperSettigs");
+            config.ReadObject<DeveloperSettigs>("DeveloperSettigs");
         }
 
         [TestMethod]
         [ExpectedException(typeof(ConfigException))]
-        public void ReadKeyNotFound()
+        public void ReadObjectKeyNotFound()
         {
             var config = GetService<IConfiguration>();
-            config.Read<DeveloperSettigs>("DeveloperSettigs");
+            config.ReadObject<DeveloperSettigs>("DeveloperSettigs");
         }
 
         [TestMethod]
-        public void ReadOk()
+        public void ReadObjectOk()
         {
             var config = GetService<IConfiguration>();
             config["DeveloperSettigs:ScreenSize"] = "100 inches";
@@ -40,12 +46,47 @@ namespace Celerik.NetCore.Util.Test
             config["DeveloperSettigs:PreferredMusic"] = "Ultra super dark satanic bloody metal";
             config["DeveloperSettigs:Pet"] = "Kitten";
 
-            var settings = config.Read<DeveloperSettigs>("DeveloperSettigs");
+            var settings = config.ReadObject<DeveloperSettigs>("DeveloperSettigs");
 
             Assert.AreEqual("100 inches", settings.ScreenSize);
             Assert.AreEqual("Café La Bastilla", settings.CoffeeBrand);
             Assert.AreEqual("Ultra super dark satanic bloody metal", settings.PreferredMusic);
             Assert.AreEqual("Kitten", settings.Pet);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReadEnumFromNullConfiguration()
+        {
+            IConfiguration config = null;
+            config.ReadEnum<GuitarBrandType>("GuitaSettings:Brand");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigException))]
+        public void ReadEnumKeyNotFound()
+        {
+            var config = GetService<IConfiguration>();
+            config.ReadEnum<GuitarBrandType>("GuitaSettings:Brand");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ConfigException))]
+        public void ReadEnumInvalidValue()
+        {
+            var config = GetService<IConfiguration>();
+            config["GuitaSettings:Brand"] = "Invalid";
+            config.ReadEnum<GuitarBrandType>("GuitaSettings:Brand");
+        }
+
+        [TestMethod]
+        public void ReadEnumOk()
+        {
+            var config = GetService<IConfiguration>();
+            config["GuitaSettings:Brand"] = "Cheapson";
+            var brand = config.ReadEnum<GuitarBrandType>("GuitaSettings:Brand");
+
+            Assert.AreEqual(GuitarBrandType.Cheapson, brand);
         }
     }
 }
--- source/Celerik.NetCore.Util/Utilities/ConfigUtility.cs
+++ source/Celerik.NetCore.Util/Utilities/ConfigUtility.cs
@@ -9,15 +9,17 @@ namespace Celerik.NetCore.Util
     public static class ConfigUtility
     {
         /// <summary>
-        /// Attempts to create a T instance by matching property names against
-        /// configuration keys recursively.
+        /// Reads an object from this configuration by matching property names
+        /// against configuration keys recursively.
         /// </summary>
-        /// <typeparam name="T">The type of the instance to be created.</typeparam>
+        /// <typeparam name="TObject">The type of the object to be created.</typeparam>
         /// <param name="config">Reference to the current IConfiguration instance.
         /// </param>
         /// <param name="key">The key of the configuration section.</param>
         /// <returns>Instance of type T.</returns>
-        public static T Read<T>(this IConfiguration config, string key)
+        /// <exception cref="ArgumentNullException">Config is null.</exception>
+        /// <exception cref="ConfigException">Key not found.</exception>
+        public static TObject ReadObject<TObject>(this IConfiguration config, string key)
         {
             if (config == null)
                 throw new ArgumentNullException(
@@ -29,10 +31,43 @@ namespace Celerik.NetCore.Util
                     UtilResources.Get("Common.ConfigurationKeyNotFound", key)
                 );
 
-            var instance = (T)Activator.CreateInstance(typeof(T));
+            var instance = (TObject)Activator.CreateInstance(typeof(TObject));
             configSection.Bind(instance);
 
             return instance;
+        }
+
+        /// <summary>
+        /// Reads an enumeration from this configuration based on the
+        /// enum description.
+        /// </summary>
+        /// <typeparam name="TEnum">Enum type.</typeparam>
+        /// <param name="config">Reference to the current IConfiguration instance.
+        /// </param>
+        /// <param name="key">The key of the configuration section.</param>
+        /// <returns>Enum value.</returns>
+        /// <exception cref="ArgumentNullException">Config is null.</exception>
+        /// <exception cref="ConfigException">Key not found or value is
+        /// invalid.</exception>
+        public static TEnum ReadEnum<TEnum>(this IConfiguration config, string key)
+            where TEnum : struct, IConvertible
+        {
+            if (config == null)
+                throw new ArgumentNullException(
+                    UtilResources.Get("Common.ArgumentCanNotBeNull", nameof(config)));
+
+            var section = config.GetSection(key);
+            var value = config[key];
+            var type = EnumUtility.GetValueFromDescription<TEnum>(value);
+
+            if (string.IsNullOrEmpty(value))
+                throw new ConfigException(
+                    UtilResources.Get("Common.ConfigurationKeyNotFound", key));
+            if (type.Equals(default(TEnum)))
+                throw new ConfigException(
+                    UtilResources.Get("Common.ConfigurationValueInvalid", key, value));
+
+            return type;
         }
     }
 }
